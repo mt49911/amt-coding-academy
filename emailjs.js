ddca83a534fd6e97d35ffbc0575d6e03f5f4eaad
mt49911 <emteey7711@gmail.com>
2026-07-29 06:06:41 +0100

    Add emailjs integration for registration notifications

--- emailjs.js
+++ emailjs.js
@@ -1,1 +1,15 @@
+const EMAILJS_PUBLIC_KEY = "8z-Z7IsaqiTyqJH-P";
+const EMAILJS_SERVICE_ID = "service_lb7vw47";
+const EMAILJS_TEMPLATE_ID = "template_ppvxhnk";
 
+emailjs.init(EMAILJS_PUBLIC_KEY);
+
+function sendRegistrationNotification(data) {
+  return emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
+    full_name: data.fullName,
+    email: data.email,
+    phone: data.phoneNumber,
+    country: data.country,
+    referral_source: data.referralSource || "Not specified"
+  });
+}
